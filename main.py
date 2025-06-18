e08a6e33e31fb22f6f9611ddf9582b41edb200ff
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2025-06-18 21:17:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from config import BOT_TOKEN
+from handlers.user import register_user_handlers
+from handlers.admin import register_admin_handlers
+from handlers.voice import register_voice_handlers
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+# Register all handlers
+register_user_handlers(dp)
+register_admin_handlers(dp)
+register_voice_handlers(dp)
+
+async def on_startup(_):
+    print("Ananya Bot is online!")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
